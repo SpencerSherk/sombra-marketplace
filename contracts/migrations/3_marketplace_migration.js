e923d7e3396df8ed57773d2ac2468cad5e92142c
Spencer Sherk <spencersherk@spencers-mbp.home>
2021-09-22 20:08:03 -0400

    add router address

--- contracts/migrations/3_marketplace_migration.js
+++ contracts/migrations/3_marketplace_migration.js
@@ -1,5 +1,5 @@
 const SombraMarketplace = artifacts.require("SombraMarketplace");
 
 module.exports = function (deployer) {
-  deployer.deploy(SombraMarketplace, '0x749B973d092eFfcb46f0C5f141E5aD6F6E448F37');
+  deployer.deploy(SombraMarketplace, '0x749B973d092eFfcb46f0C5f141E5aD6F6E448F37', '0x10ED43C718714eb63d5aA57B78B54704E256024E');
 };
